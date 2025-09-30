3d82b51f8b0605164ee1da04386acf6c479a786e
Carson Buttars <carsonbuttars13@gmail.com>
2025-09-29 21:17:30 -0600

    Initialize lm source code

---
+++ lm.c
@@ -0,0 +1,47 @@
+#include <gsl/gsl_multifit.h>
+#include "core.h"
+
+// dataColumn * columnHead;
+// gsl_vector * response;
+// gsl_vector * fit;
+// gsl_vector * residuals;
+// gsl_vector * coef;
+// gsl_matrix * dataMatrix;
+// gsl_matrix * covMatrix;
+// double * chisq;
+// gsl_multifit_linear_workspace * work;
+
+int main()
+{
+	int status;
+	int nrow;
+	int ncol;
+	dataColumn * columnHead;
+	gsl_vector * response;
+	gsl_vector * fit;
+	gsl_vector * residuals;
+	gsl_vector * coef;
+	gsl_matrix * dataMatrix;
+	gsl_matrix * covMatrix;
+	double chisq;
+	gsl_multifit_linear_workspace * work;
+
+	// Parse incoming csv file
+	status = read_table(&columnHead, &dataMatrix, &response);
+
+	// Extract data size
+	nrow = dataMatrix->size1;
+	ncol = dataMatrix->size2;
+
+	// Allocate remaining data
+	work = gsl_multifit_linear_alloc(nrow, ncol);
+	coef = gsl_vector_calloc(ncol);
+	covMatrix = gsl_matrix_calloc(ncol, ncol);
+
+	gsl_multifit_linear(dataMatrix, response, coef, covMatrix, &chisq,
+		     work);
+
+	gsl_vector_fprintf(stdout, coef, "%g");
+
+	return status;
+}
