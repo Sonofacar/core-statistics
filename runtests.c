305994b2b39384ede4dbac0ba67e623762861447
Carson Buttars <carsonbuttars13@gmail.com>
2025-10-24 12:00:12 -0600

    Remove broken encoding tests.

--- runtests.c
+++ runtests.c
@@ -24,44 +24,10 @@ static void test_detect_type_string(void **state)
 	assert_int_equal(detect_type(""), TYPE_STRING);
 }
 
-// encode
-int __wrap_dummy_encode(dataColumn * data, int nrow)
-{
-	function_called();
-}
-
-void __wrap_target_encode(dataColumn * data, gsl_vector * response, int nrow)
-{
-	function_called();
-}
-
-static void test_encode_call_dummy(void **state)
-{
-	dataColumn * data;
-	data->to_encode;
-	gsl_vector * response;
-	int nrow;
-
-	expect_function_call(target_encode);
-	encode(data, response, nrow, ENCODE_DUMMY);
-}
-
-static void test_encode_call_target(void **state)
-{
-	dataColumn * data;
-	gsl_vector * response;
-	int nrow;
-
-	expect_function_call(target_encode);
-	encode(data, response, nrow, ENCODE_TARGET);
-}
-
 int main(void) {
 	const struct CMUnitTest tests[] = {
 		cmocka_unit_test(test_detect_type_double),
 		cmocka_unit_test(test_detect_type_string),
-		cmocka_unit_test(test_encode_call_dummy),
-		cmocka_unit_test(test_encode_call_target),
 	};
  
 	return cmocka_run_group_tests(tests, NULL, NULL);
